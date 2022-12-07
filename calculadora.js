d5033ea531bad91410deedf92930de5923cdee56
TWillians <110108429+TWillians@users.noreply.github.com>
2022-12-07 09:38:01 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+const soma = function(a, b){
+return a + b;
+}
+console.log(soma(3, 5));
+
+
+const subtraçao = function(a, b){
+    return a - b;
+    }
+    console.log(subtraçao(3, 0));
+
+    const divisao = function(a, b){
+        return a / b;
+        }
+        console.log(divisao(100, 5));
+
+        const multiplicaçao = function(a, b){
+            return a * b;
+            }
+            console.log(multiplicaçao(2, 15));
